60007ac5c557db5c08d09c5d88cd8c7c7d359f99
invmv <invmy@outlook.com>
2024-11-26 21:13:49 +0800

    sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,19 +27,19 @@ export default defineConfig({
 			// 	  },
 			// 	},
 			// ],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
 		}),
 	],
 });
---
+++ src/content/docs/asdds.md
@@ -0,0 +1,8 @@
+---
+title: "Stop 12"
+description: "This is the 12th stop on our journey."
+---
+
+# Stop 12
+
+Content for stop 12.
