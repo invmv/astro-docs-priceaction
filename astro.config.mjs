092ae070140376242643af96ca648d4c7f3b8b68
invmv <invmy@outlook.com>
2024-11-26 15:59:16 +0800

    ft

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.invmy.us.kg',
+	integrations: [
+		starlight({
+			title: 'My Trading Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			// head: [
+			// 	// 示例：添加 Fathom 分析脚本标签。
+			// 	{
+			// 	  tag: 'script',
+			// 	  attrs: {
+			// 		src: 'https://cdn.usefathom.com/script.js',
+			// 		'data-site': 'MY-FATHOM-ID',
+			// 		defer: true,
+			// 	  },
+			// 	},
+			// ],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
